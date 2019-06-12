de14a727be2fd02060a5806d269dfa2171c2ee3d
pshuai88 <pshuai88@gmail.com>
2019-06-12 11:40:03 -0700

    rm usgs-notebook

--- plotFunction.py
+++ plotFunction.py
@@ -15,6 +15,26 @@ import matplotlib.image as mpimg
 import matplotlib.colors as mpcol
 from matplotlib.colors import LinearSegmentedColormap
 
+
+def plot_color_gradients(cmap_category, cmap_list, nrows):
+    gradient = np.linspace(0, 1, 256)
+    gradient = np.vstack((gradient, gradient))
+    
+    fig, axes = plt.subplots(nrows=nrows)
+    fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
+    axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
+
+    for ax, name in zip(axes, cmap_list):
+        ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
+        pos = list(ax.get_position().bounds)
+        x_text = pos[0] - 0.01
+        y_text = pos[1] + pos[3]/2.
+        fig.text(x_text, y_text, name, va='center', ha='right', fontsize=10)
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axes:
+        ax.set_axis_off()
+
 def grayscale_cmap(cmap):
     """Return a grayscale version of the given colormap"""
     cmap = plt.cm.get_cmap(cmap)
